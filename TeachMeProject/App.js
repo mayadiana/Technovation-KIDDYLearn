26a9a304d706563e4a2ad50a43bef37fdd9594b0
saracrisan05 <sara.crisan@gmail.com>
2023-02-02 23:12:50 +0200

    schita

--- TeachMeProject/App.js
+++ TeachMeProject/App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Another line, dude</Text>
+      <Text>Another day, another slay :)</Text>
       <StatusBar style="auto" />
     </View>
   );
