ad8351090238d47879d4692230384238f9531d4d
saracrisan05 <sara.crisan@gmail.com>
2023-02-24 12:01:09 +0200

    minor changes, a little bit of firebase adjustment

---
+++ www/db/register-db.js
@@ -0,0 +1,36 @@
+import firebase from "../db/config.js";
+
+const registerForm = document.getElementById("register-form");
+
+function RegisterUser(event) {
+    event.preventDefault();
+    const name = event.target["name"].value;
+    const email = event.target["email"].value;
+    const password = event.target["password"].value;
+    const confirmPassword = event.target["confirm-password"].value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return;
+    }
+
+    loading.show();
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(userCredential => {
+        // Signed in
+        let user = userCredential.user;
+        firebase.firestore().collection("users").set({
+            name: name,
+            email: email,
+
+        }).then(() => {
+            location = "home.html";
+        });
+   }).catch(error => {
+      loading.hide();
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+   });
+}
+
+registerForm.onsubmit = location.href.includes("doc").RegisterUser;
