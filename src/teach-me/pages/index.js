0491331681f0c04eafb777378e4deea3df170bda
Ioan Crișan <ioan.crisan@kephas-software.com>
2023-02-26 22:22:05 +0200

    completed firebase sign-in

--- src/teach-me/pages/index.js
+++ src/teach-me/pages/index.js
@@ -1,18 +1,19 @@
+import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import firebase from '../services/firebaseConfig'
+import { app } from '../services/firebase';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export default class Home extends React.Component {
     async login(event) {
         event.preventDefault();
 
-        const email = event.target["email"].value;
-        const password = event.target["password"].value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
 
         try {
-            let userCredential = await firebase
-                .auth()
-                .signInWithEmailAndPassword(email, password);
+            const auth = getAuth();
+            let userCredential = await signInWithEmailAndPassword(auth, email, password);
             // Signed in
         } catch (error) {
             alert(error.message);
@@ -29,20 +30,21 @@ export default class Home extends React.Component {
                 <div class="topContainer">
                     <p class="welcome-text">Bine ai revenit!</p>
                 </div>
-                <form id="login-form" class="login-form" onSubmit={login}>
-                    <div class="form-group AuthContainer">
-                        <label class="AuthLabel" for="user-email">Email</label>
-                        <input id="user-email" name="email" type="email" class="baseInput" />
+                <form id="login-form" class="login-form" onSubmit={e => this.login(e)}>
+                    <div class="form-group auth-container">
+                        <label class="auth-label" htmlFor="user-email">Email</label>
+                        <input class="auth-input" id="user-email" name="email" type="email" required placeholder='exemplu: ioan.popa@gmail.com' />
                     </div>
-                    <div class="form-group AuthContainer">
-                        <label class="AuthLabel" for="user-password">Parola</label>
-                        <input id="user-password" name="password" type="password" class="baseInput" />
+                    <div class="form-group auth-container">
+                        <label class="auth-label" htmlFor="user-password">Parola</label>
+                        <input class="auth-input" id="user-password" name="password" type="password" required />
                     </div>
-                    <div class="d-flex justify-content-center">
-                        <Link class="btn btn-primary smallButton" href="/home">Conectează-te</Link>
+                    <div class="form-group auth-container">
+                        <button class="btn btn-primary submit-button" type="submit">Conectează-te</button>
                     </div>
-                    <p class="text1">Nu ai cont deja?</p>
-                    <Link class="text1" href="/sign-up">Înregistrează-te</Link>
+                    <div class="form-group auth-container">
+                        <p class="auth-label">Nu ai încă cont?&nbsp;<Link href="/sign-up">Înregistrează-te</Link></p>
+                    </div>
                 </form>
             </div>
         )
--- src/teach-me/services/firebase.js
+++ src/teach-me/services/firebase.js
@@ -1,7 +1,9 @@
 // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
 import { initializeApp } from 'firebase/app';
+import { initializeAuth } from 'firebase/auth';
+import 'Firebase/auth';
 
-const firebaseConfig = {
+const firebaseCredentials = {
     apiKey: "AIzaSyBbKUyVjxshmkFPr2jjdfAMC1vvi5oufQg",
     authDomain: "com.technovation.lilmeerkats",
     databaseURL: "https://console.firebase.google.com/u/0/project/lil-meerkats/database",
@@ -9,9 +11,9 @@ const firebaseConfig = {
     storageBucket: "https://console.firebase.google.com/u/0/project/lil-meerkats/storage",
     messagingSenderId: "931282143603",
     appId: "1: 931282143603: android: 86088e424c8eedd7e59d06",
+    automaticDataCollectionEnabled: false,  // disable GDPR
 };
 
 // Initialize Firebase
-const firebase = initializeApp(firebaseConfig);
-
-export default firebase;
+export const app = initializeApp(firebaseCredentials);
+export const auth = initializeAuth(app);
