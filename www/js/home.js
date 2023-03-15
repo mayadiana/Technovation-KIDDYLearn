eea1d015827ead22f17f73d49db8d7c9acc524a7
saracrisan05 <sara.crisan@gmail.com>
2023-03-15 22:53:03 +0200

    changes

--- www/js/home.js
+++ www/js/home.js
@@ -1,9 +1,8 @@
-import { getLoggedUser } from '../Utils.js';
+import { getLoggedUser } from "../js/Utils.js";
 
 const nameLabel = document.querySelector('.nameLabel');
 
 getLoggedUser().then(user => {
     if (!user) return location = "../index.html";
     nameLabel.innerText = user.firstName + ' ' + user.lastName;
-    loading.hide();
 });
