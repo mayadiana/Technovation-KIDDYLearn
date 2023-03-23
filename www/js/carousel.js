9b361bd1783f15cdacfdfbb16257b9a11092c2e1
saracrisan05 <sara.crisan@gmail.com>
2023-03-23 10:49:18 +0200

    carousel

--- www/js/carousel.js
+++ www/js/carousel.js
@@ -6,3 +6,34 @@ const myCarouselElement = document.querySelector('carousel')
 const carousel = new bootstrap.Carousel(myCarouselElement, {
     touch: true,
 })
+
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel();
+      
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+      $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+      $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+      $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+      $("#myCarousel").carousel(3);
+    });
+      
+    // Enable Carousel Controls
+    $(".left").click(function(){
+      $("#carouselExampleControls").carousel("prev");
+    });
+    $(".right").click(function(){
+      $("#carouselExampleControls").carousel("next");
+    });
+  });
+
+  $('.carousel').carousel({
+    interval: 20000
+  })
